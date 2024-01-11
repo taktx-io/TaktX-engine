8c96f9ff6ce9689666646331348c75761484f6f1
Eric Hendriks <eric.hendriks@alliander.com>
2024-01-11 22:48:05 +0100

    Spotless

--- engine_pd/src/main/java/nl/qunit/bpmnmeister/engine/pd/ProcessDefinitionConsumer.java
+++ engine_pd/src/main/java/nl/qunit/bpmnmeister/engine/pd/ProcessDefinitionConsumer.java
@@ -4,6 +4,9 @@ import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecord;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.xml.bind.JAXBException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import nl.qunit.bpmnmeister.pd.model.Definitions;
 import nl.qunit.bpmnmeister.pd.model.ProcessDefinition;
@@ -13,10 +16,6 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
-
 @ApplicationScoped
 @RequiredArgsConstructor
 public class ProcessDefinitionConsumer {
@@ -25,16 +24,23 @@ public class ProcessDefinitionConsumer {
   private final BpmnParser bpmnParser;
 
   private Map<String, Map<String, ProcessDefinition>> processDefinitions = new HashMap<>();
+
   @Incoming("process-definition-xml-incoming")
   @Outgoing("process-definition-parsed-outgoing")
-  public OutgoingKafkaRecord<String, ProcessDefinition> consumeXml(ConsumerRecord<String, String> xmlRecord) {
+  public OutgoingKafkaRecord<String, ProcessDefinition> consumeXml(
+      ConsumerRecord<String, String> xmlRecord) {
     LOG.info("Received process definition XML: " + xmlRecord.value());
     try {
       Definitions definitions = bpmnParser.parse(xmlRecord.value());
 
-      Map<String, ProcessDefinition> hashCodeMap = processDefinitions.computeIfAbsent(xmlRecord.key(), k -> new HashMap<>());
+      Map<String, ProcessDefinition> hashCodeMap =
+          processDefinitions.computeIfAbsent(xmlRecord.key(), k -> new HashMap<>());
       if (!hashCodeMap.containsKey(definitions.getHash())) {
-        ProcessDefinition processDefinition = ProcessDefinition.builder().definitions(definitions).version(hashCodeMap.size() + 1).build();
+        ProcessDefinition processDefinition =
+            ProcessDefinition.builder()
+                .definitions(definitions)
+                .version(hashCodeMap.size() + 1)
+                .build();
         hashCodeMap.put(definitions.getHash(), processDefinition);
         return KafkaRecord.of(definitions.getProcessDefinitionId(), processDefinition);
       } else {
@@ -46,17 +52,24 @@ public class ProcessDefinitionConsumer {
     } catch (NoSuchAlgorithmException e) {
       LOG.error("No sha256 algorithm", e);
     }
-      return null;
+    return null;
   }
 
   @Incoming("process-definition-parsed-incoming")
   public void consumeParsed(ConsumerRecord<String, ProcessDefinition> processDefinitionRecord) {
-    LOG.info("Received process definition record: " + processDefinitionRecord.key() + " " + processDefinitionRecord.value());
-    ProcessDefinition previous = processDefinitions.computeIfAbsent(processDefinitionRecord.key(), k -> new HashMap<>()).put(processDefinitionRecord.value().getDefinitions().getHash(), processDefinitionRecord.value());
+    LOG.info(
+        "Received process definition record: "
+            + processDefinitionRecord.key()
+            + " "
+            + processDefinitionRecord.value());
+    ProcessDefinition previous =
+        processDefinitions
+            .computeIfAbsent(processDefinitionRecord.key(), k -> new HashMap<>())
+            .put(
+                processDefinitionRecord.value().getDefinitions().getHash(),
+                processDefinitionRecord.value());
     if (previous != null) {
       LOG.info("Process definition already exists");
     }
   }
-
-
 }
