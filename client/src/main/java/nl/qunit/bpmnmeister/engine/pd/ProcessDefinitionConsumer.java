4fe3d6106abecb48254116dcac36cba03dcb7204
Eric Hendriks <eric.hendriks@alliander.com>
2024-01-26 23:58:33 +0100

    Run process instances

--- client/src/main/java/nl/qunit/bpmnmeister/engine/pd/ProcessDefinitionConsumer.java
+++ client/src/main/java/nl/qunit/bpmnmeister/engine/pd/ProcessDefinitionConsumer.java
@@ -1,36 +1,31 @@
 package nl.qunit.bpmnmeister.engine.pd;
 
-import io.smallrye.reactive.messaging.kafka.KafkaConsumerRebalanceListener;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import nl.qunit.bpmnmeister.pd.model.ProcessDefinition;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
+import nl.qunit.bpmnmeister.pd.model.ProcessDefinition;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class ProcessDefinitionConsumer {
-  @Inject
-  Deployer deployer;
+  @Inject Deployer deployer;
 
   @Incoming("process-definition-parsed-incoming")
   public void consumeParsedProcessDefinition(ProcessDefinition processDefinition) {
-    String receivedProcessDefinitionId = processDefinition.getDefinitions().getProcessDefinitionId();
+    String receivedProcessDefinitionId =
+        processDefinition.getDefinitions().getProcessDefinitionId();
     String generation = processDefinition.getDefinitions().getGeneration();
-    Map<String, String> genMap = deployer.getDefinitionMap().getOrDefault(receivedProcessDefinitionId, new HashMap<>());
+    Map<String, String> genMap =
+        deployer.getDefinitionMap().getOrDefault(receivedProcessDefinitionId, new HashMap<>());
     String filenameMatchingReceivedProcessDefinition = genMap.get(generation);
     if (filenameMatchingReceivedProcessDefinition != null) {
 
-//      Properties propserties = new Properties();
-//      KafkaConsumer<String, String> consumer = new KafkaConsumer<>(propserties);
-//
-//      // Create consumer for this process definition and generation
-//      consumer.subscribe(Collections.singletonList("my-topic"));
+      //      Properties propserties = new Properties();
+      //      KafkaConsumer<String, String> consumer = new KafkaConsumer<>(propserties);
+      //
+      //      // Create consumer for this process definition and generation
+      //      consumer.subscribe(Collections.singletonList("my-topic"));
     }
   }
 }
