fe8da556fd54865e277cce16b3f178da2c8f47ac
Eric Hendriks <eric.hendriks@qunit.nl>
2025-04-02 08:16:41 +0200

    Copyright

---
+++ takt-engine/src/main/java/com/flomaestro/engine/generic/LicenseInfoConfig.java
@@ -0,0 +1,30 @@
+/*
+ *
+ *  * TaktX - A high-performance BPMN engine
+ *  * Copyright (c) 2025 TaktX B.V. All rights reserved.
+ *  * This file is part of TaktX, licensed under the TaktX Business Source License v1.0.
+ *  * Free use is permitted with up to 3 Kafka partitions. See LICENSE file for details.
+ *  * For commercial use or more partitions and features, contact [info@taktx.io] or [https://www.taktx.io/contact].
+ *
+ */
+
+package com.flomaestro.engine.generic;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import lombok.extern.slf4j.Slf4j;
+
+@ApplicationScoped
+@Slf4j
+public class LicenseInfoConfig {
+
+    void onStart(@Observes StartupEvent ev) {
+        String licenseInfo = """
+            TaktX vX.X.X - Copyright (c) 2025 [Your Company Name]
+            Licensed under TaktX Business Source License V1.0. Free use limited to 3 Kafka partitions.
+            See [https://taktx.io/license] for details. For commercial use, contact us at [https://taktx.io/contact].
+            """;
+        log.info(licenseInfo);
+    }
+}
--- takt-engine/src/main/java/com/flomaestro/engine/pd/model/BoundaryEvent.java
+++ takt-engine/src/main/java/com/flomaestro/engine/pd/model/BoundaryEvent.java
@@ -1,3 +1,13 @@
+/*
+ *
+ *  * TaktX - A high-performance BPMN engine
+ *  * Copyright (c) 2025 TaktX B.V. All rights reserved.
+ *  * This file is part of TaktX, licensed under the TaktX Business Source License v1.0.
+ *  * Free use is permitted with up to 3 Kafka partitions. See LICENSE file for details.
+ *  * For commercial use or more partitions and features, contact [info@taktx.io] or [https://www.taktx.io/contact].
+ *
+ */
+
 package com.flomaestro.engine.pd.model;
 
 import com.flomaestro.engine.pi.model.BoundaryEventInstance;
