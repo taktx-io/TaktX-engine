5399fcb4118103dd9d3aca41e376445932e8171a
Eric Hendriks <eric.hendriks@qunit.nl>
2025-12-12 09:29:30 +0100

    Cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "taktx"
 
-include("taktx-shared", "taktx-client", "taktx-engine", "taktx-runway", "taktx-client-quarkus", "taktx-ingesters:full-updates-cassandra")
+include("taktx-shared", "taktx-client", "taktx-engine", "taktx-client-quarkus")
