d2e4e849fe2f3aa2b8e63632d139f997234a3432
Eric Hendriks <eric.hendriks@qunit.nl>
2026-05-25 17:17:13 +0200

    feat: split client API into facets

---
+++ taktx-client/src/main/java/io/taktx/client/WorkersClient.java
@@ -0,0 +1,49 @@
+/*
+ * TaktX - A high-performance BPMN engine
+ * Copyright (c) 2025 Eric Hendriks
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.taktx.client;
+
+import io.taktx.CleanupPolicy;
+import java.util.Objects;
+
+/** Focused public facet for worker topic management and worker-trigger subscriptions. */
+public final class WorkersClient {
+
+  private final TaktXClient client;
+
+  WorkersClient(TaktXClient client) {
+    this.client = Objects.requireNonNull(client, "client");
+  }
+
+  public String requestExternalTaskTopic(String externalTaskId) {
+    return client.requestExternalTaskTopic(externalTaskId);
+  }
+
+  public String requestExternalTaskTopic(
+      String externalTaskId, int partitions, CleanupPolicy cleanupPolicy, short replicationFactor) {
+    return client.requestExternalTaskTopic(
+        externalTaskId, partitions, cleanupPolicy, replicationFactor);
+  }
+
+  public void registerExternalTaskConsumer(
+      ExternalTaskTriggerConsumer externalTaskTriggerConsumer, String groupId) {
+    client.registerExternalTaskConsumer(externalTaskTriggerConsumer, groupId);
+  }
+
+  public void registerUserTaskConsumer(UserTaskTriggerConsumer userTaskTriggerConsumer) {
+    client.registerUserTaskConsumer(userTaskTriggerConsumer);
+  }
+
+  public void deployTaktDeploymentAnnotatedClasses() {
+    client.deployTaktDeploymentAnnotatedClasses();
+  }
+
+  public void stopExternalTaskConsumer() {
+    client.stopExternalTaskConsumer();
+  }
+}
+
